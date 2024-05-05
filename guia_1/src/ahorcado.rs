004fdbe0adf4b61d2186ad051fb80b1fe1ba7a29
lucas-perata <lucas.perata@hotmail.com>
2024-05-05 19:18:27 -0300

    progress

--- guia_1/src/ahorcado.rs
+++ guia_1/src/ahorcado.rs
@@ -3,6 +3,9 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::vec::Vec;
 
+// TODO: Check if word already entered
+// TODO: Repeat with all the words of the list
+
 pub fn game() {
     let file_path = "./resources/ahorcado.txt";
     let mut words: Vec<String> = Vec::new();
@@ -26,20 +29,20 @@ where
 }
 
 fn compare_words(words: Vec<String>) {
-    let mut tries: usize = 0;
     // For each word
     let word = &words[0];
+    let mut tries: usize = 0;
     let mut letters: Vec<char> = Vec::new();
-    let mut user_progress: Vec<char> = Vec::new();
+    let mut user_progress: Vec<char> = vec![];
+    user_progress.resize(word.len(), '_');
     let mut user_word: String = String::new();
-    let mut check;
 
     for l in word.chars() {
         letters.push(l);
     }
 
     println!("Ingresa las letras faltantes");
-    println!("_ _ _ _");
+    println!("{:?}", user_progress);
 
     loop {
         let mut user_input: String = String::new();
@@ -47,27 +50,42 @@ fn compare_words(words: Vec<String>) {
             .read_line(&mut user_input)
             .expect("ERROR leyendo la línea");
 
-        check = letters
-            .iter()
-            .find(|&&l| l == user_input.chars().next().unwrap());
-
         if let Some(position) = word.find(user_input.chars().next().unwrap()) {
-            println!("La letra está en posición {}", position);
-            user_progress.push(user_input.chars().next().unwrap());
+            let word_index = find_position(word, user_input.chars().next().unwrap());
+            if word_index.len() > 1 {
+                let mut i: usize = 0;
+                while i < word_index.len() {
+                    user_progress.insert(word_index[i], user_input.chars().next().unwrap());
+                    i += 1;
+                }
+            } else {
+                user_progress.insert(word_index[0], user_input.chars().next().unwrap());
+
+                let _ = user_progress.remove(position + 1);
+            }
+            user_progress.truncate(word.len());
+
             user_word = user_progress.clone().into_iter().collect();
+
+            println!("Intentos restantes: {} ", tries);
         } else {
             println!("------Error------");
-            println!("Intentos restantes: {} ", 5 - tries);
+            println!("Intentos restantes: {} ", tries);
         }
 
-        println!("{}", user_word);
+        // user_progress.retain(|x| *x != '_');
 
-        if &user_word == word {
+        //println!("{:?}", user_progress);
+
+        println!("{}", user_word);
+        user_word.truncate(word.len());
+
+        if user_word.trim() == word {
             println!("Ganaste! \nLa palabra es: {}", word);
             break;
         }
 
-        if tries == 4 {
+        if tries == word.len() + 2 {
             println!("Perdiste, volvé a intentarlo");
             break;
         }
@@ -76,4 +94,14 @@ fn compare_words(words: Vec<String>) {
     }
 }
 
-fn find_position(word: &str, character: char) -> Vec<usize> {}
+fn find_position(word: &str, character: char) -> Vec<usize> {
+    let mut positions = Vec::new();
+
+    for (pos, c) in word.char_indices() {
+        if c == character {
+            positions.push(pos);
+        }
+    }
+
+    positions
+}
